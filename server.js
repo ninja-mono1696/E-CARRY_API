d7b2c43837114a022fc1fed16dd423b3baf2f671
Monojit Mondal <mondalmonojit1696@gmail.com>
2023-01-22 20:05:25 +0530

    Minor correction

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use("/api", router);
+server.use(router);
 server.listen(process.env.PORT || 8080, () => {
   console.log("JSON Server is running");
 });
